2c3e0f8a937d2a1fd97098e7a39e49553cc56369
Christian <christiandmelo@gmail.com>
2023-10-25 15:07:00 -0300

    Migração bruta dos arquivos

---
+++ Domain/ValueObjects/ConfiguracaoToken.cs
@@ -0,0 +1,17 @@
+﻿namespace Domain.ValueObjects
+{
+    public class ConfiguracaoToken
+    {
+        public ConfiguracaoToken()
+        {
+            ClientSecret = string.Empty;
+            PreSalt = string.Empty;
+            PosSalt = string.Empty;
+        }
+
+        public const string Configuration = "ConfiguracaoToken";
+        public string ClientSecret { get; set; }
+        public string PreSalt { get; set; }
+        public string PosSalt { get; set; }
+    }
+}
