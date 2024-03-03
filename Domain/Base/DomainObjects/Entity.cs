29c6c2fa72f90f1ad05a8efeaecc4c035201e2cf
Christian <christiandmelo@gmail.com>
2024-03-03 11:41:02 -0300

    Corrigindo bug do sonar

--- Domain/Base/DomainObjects/Entity.cs
+++ Domain/Base/DomainObjects/Entity.cs
@@ -30,6 +30,16 @@ namespace Domain.Base.DomainObjects
             _notificacoes?.Clear();
         }
 
+        public override bool Equals(object? obj)
+        {
+            var compareTo = obj as Entity;
+
+            if (ReferenceEquals(this, compareTo)) return true;
+            if (ReferenceEquals(null, compareTo)) return false;
+
+            return Id.Equals(compareTo.Id);
+        }
+
         public override int GetHashCode()
         {
             return GetType().GetHashCode() * 907 + Id.GetHashCode();
